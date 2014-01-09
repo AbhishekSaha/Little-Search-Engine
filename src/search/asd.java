a57838b7720605e27ed08434bc9fa0c5e5b33aaa
AbhishekSaha <as1695@scarletmail.rutgers.edu>
2014-01-08 19:44:01 -0500

    Data Structures Third Project: Search Engine
    Create and use a mini search engine of words used in multiple .txt files

---
+++ src/search/asd.java
@@ -0,0 +1,13 @@
+package search;
+
+public class asd {
+
+	/**
+	 * @param args
+	 */
+	public static int stoop(String x){
+		x.concat("hef");
+		return 3;
+	}
+
+}
---
+++ src/search/ty.java
@@ -0,0 +1,40 @@
+package search;
+
+import java.io.FileNotFoundException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+
+public class ty {
+
+	/**
+	 * @param args
+	 * @throws FileNotFoundException 
+	 */
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+	
+		String er = "uii";
+		int s = stoop(er);
+		//System.out.pri
+		
+
+}}
+
+
+
+
+
+/*
+
+LittleSearchEngine ni = new LittleSearchEngine();
+		ni.makeIndex("docs.txt", "noisewords.txt");
+		
+		for(String key: ni.keywordsIndex.keySet()){
+			System.out.println("Key: " + key);
+			System.out.println("Value: " + ni.keywordsIndex.get(key));
+		}
+*/
